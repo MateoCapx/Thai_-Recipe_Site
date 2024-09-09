10b25f846413c1cfde59ad4ba4b4cc37f135cde9
Mateocapx <mateom.realtor@gmail.com>
2024-09-09 19:45:57 -0400

    Added the search icon to the nav menu

--- thai_recipe_app/src/components/navBar.js
+++ thai_recipe_app/src/components/navBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grid2, Button, Link, Container } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 const navBar = () => {
   return (
@@ -9,25 +10,28 @@ const navBar = () => {
         <Grid2 size={{ xs: 6, md: 8 }}>
           Logo
         </Grid2>
-        <Grid2 size={{ xs: 6, md: 4,  }}>
+        <Grid2 size={{ xs: 6, md: 4, }}>
           <div id="navLinks">
-            <Link href="#" underline="none">
+            <Link href="#" underline="none" hover="" sx={{ mx: 2 }}>
               {'About'}
             </Link>
 
-            <Link href="#" underline="none">
+            <Link href="#" underline="none" sx={{ mx: 2 }}>
               {'Recipes'}
             </Link>
 
-            <Link href="#" underline="none">
+            <Link href="#" underline="none" sx={{ mx: 2 }}>
               {' Shop'}
             </Link>
 
-            <Link href="#" underline="none">
+            <Link href="#" underline="none" sx={{ mx: 2 }}>
               {'Contact'}
-            </Link>
-
+            </Link> 
+            <SearchIcon onClick={()=>{
+              console.log("Search Icon clicked")
+            }} />
           </div>
+
         </Grid2>
       </Grid2>
     </Container>

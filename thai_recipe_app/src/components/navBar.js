bd05165ec4de2920c8c81a7eb37cea3ca940f7f1
Mateocapx <mateom.realtor@gmail.com>
2024-09-10 18:53:36 -0400

    Created menu icon

--- thai_recipe_app/src/components/navBar.js
+++ thai_recipe_app/src/components/navBar.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Grid2, Button, Link, Container } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const navBar = () => {
   return (
 
     <Container id="navBarContanier">
       <Grid2 container spacing={2}>
+         
         <Grid2 size={{ xs: 6, md: 8 }}>
           Logo
+           <MenuIcon id="menuIcon"/>
         </Grid2>
         <Grid2 size={{ xs: 6, md: 4, }}>
           <div id="navLinks">
@@ -27,14 +30,18 @@ const navBar = () => {
             <Link href="#" underline="none" sx={{ mx: 2 }}>
               {'Contact'}
             </Link> 
-            <SearchIcon onClick={()=>{
+            <SearchIcon id="searchIconBtn" onClick={()=>{
               console.log("Search Icon clicked")
             }} />
           </div>
 
         </Grid2>
       </Grid2>
+
+
     </Container>
+
+    
   )
 };
 

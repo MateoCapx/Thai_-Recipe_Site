0a7ee11c8fd35c50443878c2a4366aba90a97ff2
Mateocapx <mateom.realtor@gmail.com>
2024-09-13 19:06:42 -0400

    Created Nav bar

--- thai_recipe_app/src/components/navBar.js
+++ thai_recipe_app/src/components/navBar.js
@@ -1,98 +1,73 @@
-import * as React from "react";
-import { Grid2, Button, Link, Container } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import MenuIcon from "@mui/icons-material/Menu";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import React, { useState, useEffect } from 'react';
+import { AppBar, Toolbar, IconButton, Menu, MenuItem, Button, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const NavBar = () => {
-  const [open, setOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 930);
 
-  const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
-        {["About", "Recipes", "Blog", "Videos", "Shop", "Contact"].map(
-          (text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          )
-        )}
-      </List>
-      <Divider />
-      <List>
-        {["Sign Out"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 930);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const menuItems = (
+    <>
+      <MenuItem onClick={handleMenuClose}>Home</MenuItem>
+      <MenuItem onClick={handleMenuClose}>About</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Contact</MenuItem>
+    </>
   );
 
   return (
-    <Container id="navBarContanier">
-      <Grid2 container spacing={2}>
-        <Grid2 size={{ xs: 6, md: 8 }}>
-          <div id="sideBarBtn">
-            <MenuIcon onClick={toggleDrawer(true)}>Open drawer </MenuIcon>
-            <Drawer open={open} onClose={toggleDrawer(false)}>
-              {DrawerList}
-            </Drawer>
-          </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Logo
-        </Grid2>
-        <Grid2 size={{ xs: 6, md: 4 }}>
-          <div id="navLinks">
-            <Link href="#" underline="none" hover="" sx={{ mx: 2 }}>
-              {"About"}
-            </Link>
-
-            <Link href="#" underline="none" sx={{ mx: 2 }}>
-              {"Recipes"}
-            </Link>
-
-            <Link href="#" underline="none" sx={{ mx: 2 }}>
-              {" Shop"}
-            </Link>
-
-            <Link href="#" underline="none" sx={{ mx: 2 }}>
-              {"Contact"}
-            </Link>
-            <SearchIcon
-              id="searchIconBtn"
-              onClick={() => {
-                console.log("Search Icon clicked");
-              }}
-            />
-          </div>
-        </Grid2>
-      </Grid2>
-    </Container>
+        </Typography>
+        {isMobile ? (
+          <>
+            <IconButton
+              edge="end"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleMenuOpen}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+            >
+              {menuItems}
+            </Menu>
+          </>
+        ) : (
+          <>
+            <Button color="inherit">Home</Button>
+            <Button color="inherit">About</Button>
+            <Button color="inherit">Contact</Button>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
+
+
 
 export default NavBar;
 

20f960ddca49c622c45e652af586ce452db398e1
Mateo Mcpherson <mateomcpherson@Mateos-MacBook-Pro.local>
2024-09-15 21:15:44 -0400

    styled the upload blog form

--- thai_recipe_app/src/pages/uploadBlog.js
+++ thai_recipe_app/src/pages/uploadBlog.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Typography } from "@mui/material";
+import { Button, Container, Input, TextField, Typography } from "@mui/material";
+import { red } from "@mui/material/colors";
 // import UploadBlog from "../pages/uploadBlog";
+
+const uploadBlogFiles = () => {
+  alert("File Submitted");
+};
 
 function UploadBlog() {
   return (
@@ -10,41 +15,52 @@ function UploadBlog() {
         <h1> Blogs Upload Page</h1>
         <Button component={Link} to="/" variant="contained" color="primary">
           {" "}
-          Upload Blogs
+          Back to Home Page
         </Button>
         {/* Start of Blogs Upload form */}
         <div id="BlogUpload-Form">
           <form>
-            <p>Name of Dish: </p>
-            <input
+            <br />
+
+            <span>Name of Dish: </span>
+            <Input
               type="text"
+              name="name"
+              // value={formData.name}
+              // onChange={handleChange}
+              required
+            />
+
+            <br />
+            <br />
+
+            <span>Description Of Dish: </span>
+            <TextField
+              sx={{ backgroundColor: "white" }}
+              label="Description"
+              multiline
+              rows={4} // Specifies the number of visible text lines
+              // value={description}
+            />
+
+            <br />
+            <br />
+
+            <span id="upload-photos-span">Upload Photo & Video </span>
+            <input
+              type="file"
               name="name"
               // value={formData.name}
               // onChange={handleChange}
               required
             />
             <hr />
-            <p>Description Of Dish: </p>
-            <input
-              type="text"
-              name="name"
-              // value={formData.name}
-              // onChange={handleChange}
-              required
-            />
-            <p>Upload Photo & Video: </p>
-            <input
-              type="text"
-              name="name"
-              // value={formData.name}
-              // onChange={handleChange}
-              required
-            />
-            <br />
+
             <Button
               sx={{ marginTop: "20px" }}
               variant="contained"
               color="primary"
+              onClick={uploadBlogFiles}
             >
               {" "}
               Submit Blog{" "}

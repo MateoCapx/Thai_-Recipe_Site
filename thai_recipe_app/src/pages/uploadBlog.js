364c65c8741f0171a059ced1ff2b67ee4b2f71bb
Mateo Mcpherson <mateomcpherson@Mateos-MacBook-Pro.local>
2024-09-15 20:02:21 -0400

    Working on upload blogs form

--- thai_recipe_app/src/pages/uploadBlog.js
+++ thai_recipe_app/src/pages/uploadBlog.js
@@ -1,17 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Container } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 // import UploadBlog from "../pages/uploadBlog";
 
 function UploadBlog() {
   return (
     <div>
-      <h1> Blogs Upload Page</h1>
       <Container sx={{ textAlign: "center" }}>
+        <h1> Blogs Upload Page</h1>
         <Button component={Link} to="/" variant="contained" color="primary">
           {" "}
           Upload Blogs
         </Button>
+        {/* Start of Blogs Upload form */}
+        <div id="BlogUpload-Form">
+          <form>
+            <p>Name of Dish: </p>
+            <input
+              type="text"
+              name="name"
+              // value={formData.name}
+              // onChange={handleChange}
+              required
+            />
+            <hr />
+            <p>Description Of Dish: </p>
+            <input
+              type="text"
+              name="name"
+              // value={formData.name}
+              // onChange={handleChange}
+              required
+            />
+            <p>Upload Photo & Video: </p>
+            <input
+              type="text"
+              name="name"
+              // value={formData.name}
+              // onChange={handleChange}
+              required
+            />
+            <br />
+            <Button
+              sx={{ marginTop: "20px" }}
+              variant="contained"
+              color="primary"
+            >
+              {" "}
+              Submit Blog{" "}
+            </Button>
+          </form>
+        </div>
       </Container>
     </div>
   );

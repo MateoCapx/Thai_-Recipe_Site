a367b4eaf87f58553570310dd9cfd76a8c16710e
Mateocapx <mateom.realtor@gmail.com>
2024-09-24 18:52:44 -0400

    getting interaction with database however the data isnt saving

--- backend/controller/blogController.js
+++ backend/controller/blogController.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 const blogs = require("../models/blogModels");
+const express = require('express')
+
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json()); // Add this line
 
 // Get All the Blogs
 const getAllBlogs = async (req, res) => {
@@ -8,24 +14,26 @@ const getAllBlogs = async (req, res) => {
   res.status(200).json(blogss);
 };
 
-// Create a Blog
 const createBlog = async (req, res) => {
   if (!req.body.nameOfDish && !req.body.description && !req.body.fileUpload) {
-    res.status(400);
-    throw new Error("Please fill out all fields");
+    return res.status(400).json({ message: "Please fill out all fields" });
   }
-  const blog = await blogs.create(
-    {
-      nameOfDish: req.body.nameOfDish,
-      description: req.body.description,
-      fileUpload: req.body.fileUpload,
-    },
-    console.log("Blog Post Created")
-  );
 
-  res.status(200).json(blog);
+  try {
+    const blog = new blogs({
+      nameOfDish: req.body.nameofdish,
+      description: req.body.Description,
+      fileUpload: req.body.file,
+    });
+    await blog.save();
+    console.log("Blog Post Created");
+    res.status(201).json(blog); // Use 201 for resource creation
+  } catch (error) {
+    console.error("Error creating blog:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
-createBlog();
+ 
 
 // Delete a Blogs
 const deleteBlog = async (req, res) => {

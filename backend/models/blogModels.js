cde0f7560f64fc082a09b212baefb353ec02ad17
Mateocapx <>
2024-10-01 10:26:10 -0400

    Still working

--- backend/models/blogModels.js
+++ backend/models/blogModels.js
@@ -1,22 +1,25 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const blogSchema = new Schema({
+const blogSchema = new Schema(
+  {
     nameOfDish: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
 
-    description:{
-        type: String,
-        require: true,
+    description: {
+      type: String,
+      require: true,
     },
 
-    // fileUpload:{
-    //     type: String,
-    //     require: true,
-    // }
-}, {timestamps: true})
+    fileUpload: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("BlogSchema", blogSchema )
+module.exports = mongoose.model("BlogSchema", blogSchema);
